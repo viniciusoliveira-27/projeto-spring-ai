bc14d0f768221121c65f90cce37ca9ac3996da99
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-06-02 22:42:39 -0300

    feat: Criado o service e o controller da receita

--- src/main/java/com/example/spring_ai/controller/GenerativeAIController.java
+++ src/main/java/com/example/spring_ai/controller/GenerativeAIController.java
@@ -1,6 +1,7 @@
 package com.example.spring_ai.controller;
 
 import com.example.spring_ai.service.ChatService;
+import com.example.spring_ai.service.RecipeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,19 +9,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GenerativeAIController {
 
-    private final ChatService service;
+    private final ChatService chatService;
+    private final RecipeService recipeService;
 
-    public GenerativeAIController(ChatService service) {
-        this.service = service;
+    public GenerativeAIController(ChatService chatService, RecipeService recipeService) {
+
+        this.chatService = chatService;
+        this.recipeService = recipeService;
     }
 
     @GetMapping("ask-ai")
     public String getResponse(@RequestParam String prompt) {
-        return service.getResponse(prompt);
+
+        return chatService.getResponse(prompt);
     }
     @GetMapping("ask-ai-options")
     public String getResponseWithOptions(@RequestParam String prompt) {
-        return service.getResponseWithOptions(prompt);
+
+        return chatService.getResponseWithOptions(prompt);
+    }
+    @GetMapping("recipe-creator")
+    public String recipeCreator(@RequestParam String ingredients,
+                                @RequestParam(defaultValue = "any") String cuisine,
+                                @ RequestParam(defaultValue = "none") String dietaryRestrictions) {
+
+        return recipeService.createRecipe(ingredients, cuisine, dietaryRestrictions);
     }
 
 }
