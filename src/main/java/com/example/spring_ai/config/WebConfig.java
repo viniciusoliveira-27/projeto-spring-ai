9e4bb807946d64c8602b3d2ee3dcf7e4a0b0f188
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-06-04 12:37:05 -0300

    feat: criando o WebConfig para configurar o CORS para integrar com o react

---
+++ src/main/java/com/example/spring_ai/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.spring_ai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    //mudar na parte do localhost para o dominio, quando fizer o deploy
+    @Bean //metodo, classe ou propriedade que configuramos para que o Spring utilize
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080/", "http://localhost:3000/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
