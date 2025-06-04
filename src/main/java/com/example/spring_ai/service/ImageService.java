22589cd56350490d7f4e914530f791b81d5ac520
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-06-04 11:08:17 -0300

    feat: criação do service da criação de imagem

---
+++ src/main/java/com/example/spring_ai/service/ImageService.java
@@ -0,0 +1,35 @@
+package com.example.spring_ai.service;
+
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+
+    private final OpenAiImageModel imageModel;
+
+    public ImageService(OpenAiImageModel imageModel) {
+        this.imageModel = imageModel;
+    }
+
+    public ImageResponse generateImage(String prompt,
+                                       String quality,
+                                       Integer n,
+                                       Integer height,
+                                       Integer width) {
+
+        ImageResponse imageResponse = imageModel.call(
+                new ImagePrompt(prompt,
+                        OpenAiImageOptions.builder()
+                                .quality(quality)
+                                .N(n)
+                                .height(height)
+                                .width(width).build())
+
+        );
+        return imageResponse;
+    }
+}
