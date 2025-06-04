41731eb142215a201927835f47eebae5b74c4a62
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-06-04 12:21:26 -0300

    feat: criando o controller da criação de imagem

--- src/main/java/com/example/spring_ai/controller/GenerativeAIController.java
+++ src/main/java/com/example/spring_ai/controller/GenerativeAIController.java
@@ -1,10 +1,14 @@
 package com.example.spring_ai.controller;
 
 import com.example.spring_ai.service.ChatService;
+import com.example.spring_ai.service.ImageService;
 import com.example.spring_ai.service.RecipeService;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GenerativeAIController {
@@ -12,10 +16,15 @@ public class GenerativeAIController {
     private final ChatService chatService;
     private final RecipeService recipeService;
 
-    public GenerativeAIController(ChatService chatService, RecipeService recipeService) {
+    private final ImageService imageService;
+
+    public GenerativeAIController(ChatService chatService,
+                                  RecipeService recipeService,
+                                  ImageService imageService) {
 
         this.chatService = chatService;
         this.recipeService = recipeService;
+        this.imageService = imageService;
     }
 
     @GetMapping("ask-ai")
@@ -31,9 +40,23 @@ public class GenerativeAIController {
     @GetMapping("recipe-creator")
     public String recipeCreator(@RequestParam String ingredients,
                                 @RequestParam(defaultValue = "any") String cuisine,
-                                @ RequestParam(defaultValue = "none") String dietaryRestrictions) {
+                                @RequestParam(defaultValue = "none") String dietaryRestrictions) {
 
         return recipeService.createRecipe(ingredients, cuisine, dietaryRestrictions);
+    }
+
+    @GetMapping("generate-image")
+    public List<String> generateImages(@RequestParam String prompt,
+                                         @RequestParam(defaultValue = "standard") String quality,
+                                         @RequestParam(defaultValue = "1")Integer n,
+                                         @RequestParam(defaultValue = "1024")Integer height,
+                                         @RequestParam(defaultValue = "1024")Integer width) {
+
+        ImageResponse response = imageService.generateImage(prompt, quality, n, height, width);
+        List<String> imageUrls = response.getResults().stream()
+                    .map(result -> result.getOutput().getUrl())
+                .toList();
+        return imageUrls;
     }
 
 }
